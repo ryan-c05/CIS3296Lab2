2159725dfc4865d465eca1a3f9d532062a4efb47
Ryan Chan <ryanmrchan@gmail.com>
2026-09-02 16:02:24 -0400

    Modified the Withdraw method to be able to withdraw from balance when ending balance is at least  and charges a  withdraw fee.

--- BankAccount.java
+++ BankAccount.java
@@ -12,12 +12,13 @@ public class BankAccount {
     }
 
     public boolean withdraw(double amount) {
-        // TODO:
-        // Withdraw the requested amount if sufficient funds are available.
-        // Return true if the withdrawal succeeds.
-        // Return false otherwise.
-        return false;
+        if (balance - amount >= 50) {
+            balance = (balance - amount) - 2;   //deduct $2 fee for withdrawal
+            return true;
         }
+        
+        return false;
+    }
 
     public String getAccountSummary() {
         return accountHolder + ": $" + balance;
